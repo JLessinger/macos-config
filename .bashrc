cfae24e84393c9edfec113eedbbf8bd5ec08ccd8
Jonathan Lessinger <jonathan.lessinger@gmail.com>
2016-06-13 20:24:47 -0400

    basic mac + ubuntu

---
+++ .bashrc
@@ -0,0 +1,51 @@
+export EDITOR=`which emacs`
+
+function cl {
+    DIR=$1
+    if [ -z "$1" ]; then
+       DIR=$HOME
+    fi      
+    cd "$DIR" && ls;
+}
+
+function clean {
+    DIR="$1"
+    if [ -z "$1" ]; then
+	DIR="."
+    fi
+    rm $DIR/*~
+}
+
+function cleanr {
+    DIR="$1"
+    if [ -z "$1" ]; then
+	DIR="."
+    fi
+    find "$DIR" -name "*~" -delete
+}
+
+#java
+
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_12.jdk/Contents/Home"
+export ANT_HOME="/usr/local/Cellar/ant/1.9.2/libexec"
+
+test -r /sw/bin/init.sh && . /sw/bin/init.sh
+
+# Setting PATH for Python 2.7
+# The orginal version is saved in .profile.pysave
+PATH="/Library/Frameworks/GTK+.framework/Versions/2.18.x11/Resources/bin:${PATH}"
+
+## use anaconda libraries instead
+#export PYTHONPATH="/usr/local/lib/python2.7/site-packages:/Library/Python/2.7/site-packages/"
+#export PYTHONPATH="$PYTHONPATH:/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python/"
+
+## can we use both, defaulting to anaconda? this particular setting doesn't work
+#export PYTHONPATH="/Users/jonathan/Library/anaconda2/lib/:$PYTHONPATH"
+
+####aliases
+
+
+export DYLD_LIBRARY_PATH=/usr/local/mysql-5.5.13-osx10.6-x86_64/lib/
+
+# added by Anaconda2 4.0.0 installer
+export PATH="/Users/jonathan/Library/anaconda2/bin:$PATH"
---
+++ bash_setup.sh
@@ -0,0 +1,97 @@
+USG_MSG="usage: bash_setup.sh [-b] -s (mac|ubuntu)
+\t-b : bypass interactive confirmation; just execute\n"
+
+function abort {
+    printf "$1\n" >&2
+    exit 1
+}
+
+#  in:
+#    nothing
+#  out:
+#    nothing
+#  side effects:
+#    cancels with status 0 iff not confirmed
+function confirm {
+    if [ "$BYPASS" == false ] ; then
+	printf "WARNING: overwrites stuff. Set up unix environment? (y/n)\n"
+	read resp
+	resp=${resp,,}
+	case "$resp" in
+	    y|ye|yes) # pass through
+	    ;;
+	    *) printf "canceling\n"
+	       exit 0
+	       ;;
+	esac
+    fi
+}
+
+function execute_mac {
+    cp .bashrc ~/
+    cp .bash_profile ~
+    sudo /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    sudo brew install emacs
+}
+
+function execute_ubuntu {
+    cp .bashrc ~/
+    cp .bash_profile ~
+    sudo apt-get install emacs
+}
+
+
+function confirm_execute {
+    confirm
+    if [ "$SYSTEM" == "mac" ]; then
+	execute_mac
+    elif [ "$SYSTEM" == "ubuntu" ]; then
+	execute_ubuntu
+    else
+	# shouldn't be here
+    fi
+    
+}
+
+function verify_confirm_execute {
+    if [ "$SYSTEM" == "mac" ] || [ "$SYSTEM" == "ubuntu" ]; then
+	confirm_execute "$@"
+    else
+	abort "$USG_MSG"
+    fi
+}
+
+function parse_verify_confirm_execute {
+    OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+    # Initialize our own variables:
+    BYPASS=false
+    REPLICATE=false
+    SYSTEM=""
+
+    while getopts "hbri:" opt; do
+	case "$opt" in
+	    # fill in options
+	    s)
+		SYSTEM=$OPTARG ;;
+	    b)
+		BYPASS=true ;;
+            h)
+		printf "$USG_MSG"
+		exit 0 ;;
+	    *)
+		abort "$USG_MSG"
+	esac
+    done
+
+    shift $((OPTIND-1))
+
+    [ "$1" = "--" ] && shift
+
+    # get rid of trailing / to make the rest easier
+    args=( "$@" )
+    args[0]="${1%/}"
+    args[1]="${2%/}"
+    set "${args[@]}"
+    verify_confirm_execute "$@"
+}
