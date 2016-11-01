22b948d7794213e4aa80a5e1dc31ecbdc580b930
Jonathan Lessinger <jonathan.lessinger@gmail.com>
2016-10-31 17:42:43 -0700

    [EMACS] various ediff / library stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,15 +4,25 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
+(load-file "~/.emacs.d/local_lisp/global.el")
 
-(add-to-list 'load-path "~/.emacs.d/local_lisp/")
-(load-library "global")
+(use-package try)
+
+(use-package which-key
+  :config (which-key-mode))
+
+(use-package org-bullets
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(org-agenda-files
    (quote
     ("~/Documents/notes/shared-notes/sh_main.org" "~/Documents/notes/main.org")))
