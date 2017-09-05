95ddbc420f8943e289a0a5f2f29c61c7c960c3b6
Jonathan Lessinger <jonathan.lessinger@gmail.com>
2017-09-04 21:28:26 -0400

    add initial hammerspoon config

---
+++ hammerspoon/init.lua
@@ -0,0 +1,7 @@
+os.execute('rm /usr/local/bin/hs')
+os.execute('rm /usr/local/share/man/man1/hs.1')
+ipc = require('hs.ipc')
+ipc.cliInstall()
+
+remap_l_up = hs.hotkey.bind({'cmd'}, 'l', function() hs.eventtap.keyStroke({'cmd'}, 'up') end)
+f_real_l = hs.hotkey.bind({'ctrl'}, 'f', function() remap_l_up:disable(); hs.eventtap.keyStroke({'cmd'}, 'l'); remap_l_up:enable() end)
