bb800c7acb7f4b5bcbb8191ba7213c8816894a47
Jonathan Lessinger <jonathan.lessinger@gmail.com>
2017-09-11 12:51:09 -0400

    some test hammerspoon  crap

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,5 +3,5 @@ os.execute('rm /usr/local/share/man/man1/hs.1')
 ipc = require('hs.ipc')
 ipc.cliInstall()
 
-remap_l_up = hs.hotkey.bind({'cmd'}, 'l', function() hs.eventtap.keyStroke({'cmd'}, 'up') end)
-f_real_l = hs.hotkey.bind({'ctrl'}, 'f', function() remap_l_up:disable(); hs.eventtap.keyStroke({'cmd'}, 'l'); remap_l_up:enable() end)
+-- remap_l_up = hs.hotkey.bind({'cmd'}, 'l', function() hs.eventtap.keyStroke({'cmd'}, 'up') end)
+-- f_real_l = hs.hotkey.bind({'ctrl'}, 'f', function() remap_l_up:disable(); hs.eventtap.keyStroke({'cmd'}, 'l'); remap_l_up:enable() end)
